baca025706a0c87cab34612c4c69b61b8aa9b9d5
Natalie <njseeto@users.noreply.github.com>
2020-02-15 12:13:06 +0000

    feat(allowed ips): add variables for allowed ips.

--- config.py
+++ config.py
@@ -7,6 +7,8 @@ month = now.strftime("%m")
 day = now.strftime("%d")
 get_last_hour = datetime.now() - timedelta(hours = 1)
 last_hour = (get_last_hour.strftime('%H'))
+allowed_ip_range = ''
+allowed_ip_range_2 = ''
 
 # Add your bucket and key here
 bucket = ''
